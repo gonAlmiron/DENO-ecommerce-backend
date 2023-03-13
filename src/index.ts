5eadd152494ce3cafc24978a0f8b067d7bad1bd9
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-03-13 15:37:12 -0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('BUUUUUENAS')
+import * as serve from "https://deno.land/x/serve@1.0.5/deps.ts";
+
+const PORT = 8080
+
+serve.listen(PORT, () => {
+    console.log(`Servidor ONLINE escuchando en puerto ${PORT}`)
+})
+
+
+
